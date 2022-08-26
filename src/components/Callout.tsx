e7352fa5f8e4f3c9522fdb644d251f0eb7e7d03e
Akash Hamirwasia <akash.hamirwasia@razorpay.com>
2022-08-26 19:50:38 +0530

    feat: add Callout component

---
+++ src/components/Callout.tsx
@@ -0,0 +1,7 @@
+const Callout = ({ children }: { children: React.ReactNode }) => (
+  <div className="bg-white border-t-4 border-blue-600">
+    <div className="container mx-auto px-4 py-32 text-center ">{children}</div>
+  </div>
+);
+
+export default Callout;
