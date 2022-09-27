11dffbde56029d9a4b9cadc1e074bdeee20de4c5
Nemo <nemo@razorpay.com>
2022-09-27 17:01:42 +0530

    Add one more link to public presentations repo

--- src/components/Talks/Talks.tsx
+++ src/components/Talks/Talks.tsx
@@ -14,7 +14,7 @@ const Talks = ({ talks }: { talks: Talk[] }) => (
     <div className="pb-32 pt-20 md:pt-8 bg-slate-900 bg-dotted relative z-10 -mt-px">
       <div className="container mx-auto px-4">
         <h1 className="bg-clip-text bg-gradient-to-r from-blue-100 to-blue-200 text-transparent text-5xl font-bold inline-block">
-          Public Talks
+          <a href="https://github.com/razorpay/public-presentations">Public Talks</a>
         </h1>
       </div>
       <div className="mt-20 grid md:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-8 container mx-auto px-4">
