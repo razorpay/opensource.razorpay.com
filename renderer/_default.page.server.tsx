9b23d996edac90046a7fc874b3ef59035b8b777b
Akash Hamirwasia <akash.hamirwasia@razorpay.com>
2022-09-27 18:40:34 +0530

    fix: file extension of site-preview

--- renderer/_default.page.server.tsx
+++ renderer/_default.page.server.tsx
@@ -38,14 +38,14 @@ async function render(pageContext: PageContextBuiltIn & PageContext) {
         <meta property="og:url" content="https://opensource.razorpay.com/">
         <meta property="og:title" content="${title}">
         <meta property="og:description" content="${desc}">
-        <meta property="og:image" content="https://opensource.razorpay.com/site-preview.png">
+        <meta property="og:image" content="https://opensource.razorpay.com/site-preview.jpg">
         
         <!-- Twitter -->
         <meta property="twitter:card" content="summary_large_image">
         <meta property="twitter:url" content="https://opensource.razorpay.com/">
         <meta property="twitter:title" content="${title}">
         <meta property="twitter:description" content="${desc}">
-        <meta property="twitter:image" content="https://opensource.razorpay.com/site-preview.png">
+        <meta property="twitter:image" content="https://opensource.razorpay.com/site-preview.jpg">
 
         <link rel="icon" type="image/png" href="https://opensource.razorpay.com/favicon.png">
 
